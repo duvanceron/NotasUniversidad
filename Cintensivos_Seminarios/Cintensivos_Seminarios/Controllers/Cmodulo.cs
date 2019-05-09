71a94659fde18e5be373c90615f32c6c1384a421
duvanceron <duvanceroon@hotmail.com>
2019-05-08 22:10:04 -0500

    Modulos

---
+++ Cintensivos_Seminarios/Cintensivos_Seminarios/Controllers/Cmodulo.cs
@@ -0,0 +1,24 @@
+﻿using Cintensivos_Seminarios.Models;
+using System.Data;
+
+namespace Cintensivos_Seminarios.Controllers
+{
+	public class Cmodulo
+	{
+		Mmodulo modelModulo;
+		Mgrupo modelGrupo;
+		public Cmodulo()
+		{
+			modelModulo = new Mmodulo();
+			modelGrupo = new Mgrupo();
+
+		}
+
+		public DataTable ConsultarModulos(Cgrupo ctrlGrupo)
+		{
+			modelGrupo.codigo = ctrlGrupo.codigo;
+
+			return modelModulo.ConsultarModulos(modelGrupo);
+		}
+	}
+}
--- Cintensivos_Seminarios/Cintensivos_Seminarios/Models/Mmodulo.cs
+++ Cintensivos_Seminarios/Cintensivos_Seminarios/Models/Mmodulo.cs
@@ -17,9 +17,10 @@ namespace Cintensivos_Seminarios.Models
 			parameter = new Parametro[2];
 			parameter[0] = new Parametro("CURSOR1", "", "CURSOR", ParameterDirection.ReturnValue);
 			parameter[1] = new Parametro("GRUPO", modelGrupo.codigo, "NUMBER", ParameterDirection.Input);
-
 			return conn.OraProcedimiento("FN_CONSULTARMODULOS",parameter);
 		}
+
+		
 
 
 	}
